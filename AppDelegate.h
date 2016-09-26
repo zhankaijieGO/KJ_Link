70ab475c94b416e4e16f025cd5d6dfba49b89424
湛凯杰 <793103155@qq.com>
2016-09-26 15:36:18 +0800

    创建项目试试
    第一次用，试试效果。。

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KJ_Link
+//
+//  Created by 湛凯杰 on 16/9/26.
+//  Copyright © 2016年 湛凯杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KJ_Link
+//
+//  Created by 湛凯杰 on 16/9/26.
+//  Copyright © 2016年 湛凯杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
